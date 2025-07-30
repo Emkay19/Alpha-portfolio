858a62bc631cc5ba2c110aecfea109a2250d8693
Gift Mkhoma <165139537+Emkay19@users.noreply.github.com>
2025-07-30 12:13:20 +0200

    Made a few changes

---
+++ portfolio/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div class="content">
+      <h1 class="name">Sinethemba Mkhoma</h1>
+      <h2 class="role">ICT Student</h2>
+    </div>
+  </header>
+
+  <section class="about">
+    <h2 class="aboutMe">About Me</h2>
+    <p class="abt">
+      "I am a passionate 2nd year student who is good at making figma projects and I can code a webisite from the ground
+      up using vue, html and css"
+    </p>
+  </section>
+
+  <section class="projects">
+    <h2 class="myProjects">My Projects</h2>
+  </section>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+html {
+  background-color: rgb(209, 205, 196);
+}
+
+h1 {
+  font-size: larger;
+  text-align: left;
+  color: white;
+}
+</style>
